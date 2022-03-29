61f88d8c097da65989d76df3b164b5dab3cff624
Lorenzo labihi Garibaldi <mekhipipher@gmail.com>
2022-03-29 08:59:12 +0200

    Added toggle read status functionality

--- main.js
+++ main.js
@@ -2,18 +2,34 @@ const bookShelf = document.getElementsByClassName("bookshelf")[0];
 
 let myLibrary = [];
 
-function Book(title,author,pagesNumber, isRead){
+function Book(title, author, pagesNumber, isRead) {
     this.title = title;
     this.author = author;
     this.pagesNumber = pagesNumber;
-    this.isRead = isRead; 
+    this.isRead = isRead;
 }
 
-function removeBookFromLibrary(title){
-    myLibrary.splice(myLibrary.findIndex(a => a.title === title));
+function removeBookFromLibrary(title) {
+    myLibrary.splice(myLibrary.findIndex((a) => a.title === title));
 }
 
-function addBookToLibrary(book){
+function toggleReadStatus() {
+	let index = this.dataset.bookIndex;
+    myLibrary[index].isRead =
+        !myLibrary[index].isRead;
+    console.log(myLibrary[index].isRead);
+    if (myLibrary[index].isRead) {
+        this.innerText = "Read";
+        this.classList.add("read");
+        this.classList.remove("not-read");
+    } else {
+        this.innerText = "Not Read";
+        this.classList.add("not-read");
+        this.classList.remove("read");
+    }
+}
+
+function addBookToLibrary(book) {
     myLibrary.push(book);
     const bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
@@ -41,23 +57,34 @@ function addBookToLibrary(book){
     removeButton.classList.add("remove-book");
     let index = (element) => element.title === book.title;
     readButton.dataset.bookIndex = myLibrary.findIndex(index);
+    readButton.addEventListener("click", toggleReadStatus);
     removeButton.dataset.bookIndex = myLibrary.findIndex(index);
+    removeButton.innerText = "Remove";
+    if (book.isRead) {
+        readButton.innerText = "Read";
+        readButton.classList.add("read");
+        readButton.classList.remove("not-read");
+    } else {
+        readButton.innerText = "Not Read";
+        readButton.classList.add("not-read");
+        readButton.classList.remove("read");
+    }
     bookButtons.appendChild(readButton);
     bookButtons.appendChild(removeButton);
     bookCard.appendChild(bookButtons);
 }
 
-function createBook(){
-    const title = document.getElementById("title").value; 
+function createBook() {
+    const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pagesNumber = document.getElementById("pagesNumber").value;
-    const isRead = document.getElementById("isRead").checked; 
-    const newBook = new Book(title,author,pagesNumber,isRead);
+    const isRead = document.getElementById("isRead").checked;
+    const newBook = new Book(title, author, pagesNumber, isRead);
     addBookToLibrary(newBook);
 }
 
-function listBooks(){
-    myLibrary.forEach(book => {
+function listBooks() {
+    myLibrary.forEach((book) => {
         console.log(book);
     });
 }
@@ -70,20 +97,19 @@ let btn = document.getElementById("add-book");
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = function () {
+    modal.style.display = "none";
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
