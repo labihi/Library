b3452dd6caf7157e8e19c7a27b059f5d1a291612
Lorenzo Garibaldi <mekhipipher@gmail.com>
2022-07-26 21:54:06 +0200

    Updated the project to use some basic classes

--- main.js
+++ main.js
@@ -2,96 +2,109 @@ const bookShelf = document.getElementsByClassName("bookshelf")[0];
 
 let myLibrary = [];
 
-function Book(title, author, pagesNumber, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pagesNumber = pagesNumber;
-    this.isRead = isRead;
-}
-
-function removeBookFromLibrary(title) {
-    myLibrary.splice(myLibrary.findIndex((a) => a.title === title));
-	this.parentElement.parentElement.remove();
-}
-
-function toggleReadStatus() {
-	let index = this.dataset.bookIndex;
-    myLibrary[index].isRead =
-        !myLibrary[index].isRead;
-    console.log(myLibrary[index].isRead);
-    if (myLibrary[index].isRead) {
-        this.innerText = "Read";
-        this.classList.add("read");
-        this.classList.remove("not-read");
-    } else {
-        this.innerText = "Not Read";
-        this.classList.add("not-read");
-        this.classList.remove("read");
+class Book{
+    constructor(title, author, pagesNumber, isRead){
+        this.title = title;
+        this.author = author;
+        this.pagesNumber = pagesNumber;
+        this.isRead = isRead;
     }
 }
 
 
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-    bookShelf.appendChild(bookCard);
-    const bookInfo = document.createElement("div");
-    bookInfo.classList.add("book-info");
-    bookCard.appendChild(bookInfo);
-    const title = document.createElement("p");
-    title.classList.add("title");
-    const author = document.createElement("p");
-    author.classList.add("author");
-    const pages = document.createElement("p");
-    pages.classList.add("pages");
-    title.textContent = "Title: " + book.title;
-    author.textContent = "Author: " + book.author;
-    pages.textContent = "Pages: " + book.pagesNumber;
-    bookInfo.appendChild(title);
-    bookInfo.appendChild(author);
-    bookInfo.appendChild(pages);
-    const bookButtons = document.createElement("div");
-    bookButtons.classList.add("book-buttons");
-    const readButton = document.createElement("button");
-    readButton.classList.add("read-book");
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("remove-book");
-    let index = (element) => element.title === book.title;
-    readButton.dataset.bookIndex = myLibrary.findIndex(index);
-    readButton.addEventListener("click", toggleReadStatus);
-    removeButton.dataset.bookIndex = myLibrary.findIndex(index);
-    removeButton.innerText = "Remove";
-	removeButton.addEventListener("click",removeBookFromLibrary);
-    if (book.isRead) {
-        readButton.innerText = "Read";
-        readButton.classList.add("read");
-        readButton.classList.remove("not-read");
-    } else {
-        readButton.innerText = "Not Read";
-        readButton.classList.add("not-read");
-        readButton.classList.remove("read");
+class Library{
+
+    removeBookFromLibrary(title) {
+        myLibrary.splice(myLibrary.findIndex((a) => a.title === title));
+        this.parentElement.parentElement.remove();
     }
-    bookButtons.appendChild(readButton);
-    bookButtons.appendChild(removeButton);
-    bookCard.appendChild(bookButtons);
+    
+    toggleReadStatus() {
+        let index = this.dataset.bookIndex;
+        myLibrary[index].isRead =
+            !myLibrary[index].isRead;
+        console.log(myLibrary[index].isRead);
+        if (myLibrary[index].isRead) {
+            this.innerText = "Read";
+            this.classList.add("read");
+            this.classList.remove("not-read");
+        } else {
+            this.innerText = "Not Read";
+            this.classList.add("not-read");
+            this.classList.remove("read");
+        }
+    }
+
+     
+    addBookToLibrary(book) {
+        myLibrary.push(book);
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        bookShelf.appendChild(bookCard);
+        const bookInfo = document.createElement("div");
+        bookInfo.classList.add("book-info");
+        bookCard.appendChild(bookInfo);
+        const title = document.createElement("p");
+        title.classList.add("title");
+        const author = document.createElement("p");
+        author.classList.add("author");
+        const pages = document.createElement("p");
+        pages.classList.add("pages");
+        title.textContent = "Title: " + book.title;
+        author.textContent = "Author: " + book.author;
+        pages.textContent = "Pages: " + book.pagesNumber;
+        bookInfo.appendChild(title);
+        bookInfo.appendChild(author);
+        bookInfo.appendChild(pages);
+        const bookButtons = document.createElement("div");
+        bookButtons.classList.add("book-buttons");
+        const readButton = document.createElement("button");
+        readButton.classList.add("read-book");
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("remove-book");
+        let index = (element) => element.title === book.title;
+        readButton.dataset.bookIndex = myLibrary.findIndex(index);
+        readButton.addEventListener("click", library.toggleReadStatus);
+        removeButton.dataset.bookIndex = myLibrary.findIndex(index);
+        removeButton.innerText = "Remove";
+        removeButton.addEventListener("click",library.removeBookFromLibrary);
+        if (book.isRead) {
+            readButton.innerText = "Read";
+            readButton.classList.add("read");
+            readButton.classList.remove("not-read");
+        } else {
+            readButton.innerText = "Not Read";
+            readButton.classList.add("not-read");
+            readButton.classList.remove("read");
+        }
+        bookButtons.appendChild(readButton);
+        bookButtons.appendChild(removeButton);
+        bookCard.appendChild(bookButtons);
+    }
+    
+    createBook() {
+        const title = document.getElementById("title").value;
+        const author = document.getElementById("author").value;
+        const pagesNumber = document.getElementById("pagesNumber").value;
+        const isRead = document.getElementById("isRead").checked;
+        const newBook = new Book(title, author, pagesNumber, isRead);
+        library.addBookToLibrary(newBook);
+    }
+    
+    listBooks() {
+        myLibrary.forEach((book) => {
+            console.log(book);
+        });
+    }
 }
 
-function createBook() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-    const pagesNumber = document.getElementById("pagesNumber").value;
-    const isRead = document.getElementById("isRead").checked;
-    const newBook = new Book(title, author, pagesNumber, isRead);
-    addBookToLibrary(newBook);
-}
+let library = new Library();
 
-function listBooks() {
-    myLibrary.forEach((book) => {
-        console.log(book);
-    });
-}
+
+
+
+
 
 let modal = document.getElementById("myModal");
 
